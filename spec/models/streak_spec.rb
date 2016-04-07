d9b4de8476381fffa1bca2e0a19c2a2d71666886
Jack Yeh <jack.yeh@gmail.com>
2016-04-06 22:12:35 -0600

    create/pass tests: streak has description, as well as default values for attributes

--- spec/models/streak_spec.rb
+++ spec/models/streak_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Streak, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:streak) {Streak.create!(description: 'Exercise')}
+
+  context '#streaks' do
+    it 'has a description' do
+      expect(streak.description).to eq('Exercise')
+    end
+
+    it 'has a default value for total days, longest streak and current streak of 0' do
+      expect(streak.total_days).to eq(0)
+      expect(streak.longest_streak).to eq(0)
+      expect(streak.current_streak).to eq(0)
+    end
+  end
 end
